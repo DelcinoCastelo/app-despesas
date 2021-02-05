c222af821a83e6e21b3af6ca5df860a6c5418f25
Delcino Castelo <delcinocastelo@gmail.com>
2021-02-05 22:40:32 +0100

    Commit de Fevereiro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:despesas/components/home.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/services.dart'; // primeiro
 
 main() => runApp(ExpensesApp());
 
